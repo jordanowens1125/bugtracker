d05f858a620e411ae8d3f5b315fc1c35d0cc5b94
Jordan Owens <jordanowens1125@gmail.com>
2023-06-12 14:05:57 -0500

    Added persist res 200 status on api call

--- server/routes/persist.js
+++ server/routes/persist.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const { persist } = require("../controllers/persist");
-
-router.get("/", persist);
+router.get("/", (req, res) => {
+    res.status(200).json();
+});
 
 module.exports = router;
